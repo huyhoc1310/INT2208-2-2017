92f859eda61523c90ff93a2857ff9474375ccd03
huyhoc1310 <huyhoc1310@gmail.com>
2017-04-25 22:34:35 +0700

    thêm http service

--- Nhom-Corgi/project/src/app/onlinetest.component.ts
+++ Nhom-Corgi/project/src/app/onlinetest.component.ts
@@ -1,17 +1,38 @@
 import { Component } from '@angular/core';
-
+import { OnlineTestService} from './services/onlinetest.service';
 @Component({
     selector:'my-exam',
-    templateUrl:'onlinetest.component.html'
+    templateUrl:'onlinetest.component.html',
+    providers: [OnlineTestService]
 })
 export class OnlineTestComponent{
-    correctAnswerCount = 0;
-    show = false;
-    
-    public clickAnswer() {
-        this.correctAnswerCount += 1;
-    }   
-    public clickSubmit() {
-        this.show = true;
+    public questions: Object[];
+    constructor(private onlinetestService: OnlineTestService){
+
+    }
+    ngOnInit(){
+        this.onlinetestService.GetList().subscribe((response:any)=>{
+            this.questions=response;
+        });
+    }
+    countCorrectAnswer: number;
+    saveStatus(question: Object, value:String) {
+      question['option'] = value;
+    }
+    saveValue(question: Object, value: String){
+      question['option'] = value;
+    }
+    check() {
+      this.countCorrectAnswer = 0;
+
+      for (let i = 1; i < 51; i ++){
+        if(this.questions[i]['option'] == this.questions[i]['correct_answer']){
+          this.countCorrectAnswer++;
+          this.questions[i]['status'] = "Right";
+        }else{
+          this.questions[i]['status'] = "Wrong! The correct answer is " + this.questions[i]['correct_answer'];
+        }
+      }
+      console.log('dmmay');
     }
 }  
