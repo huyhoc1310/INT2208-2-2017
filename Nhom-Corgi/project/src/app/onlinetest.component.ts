f8de046cc00e3860553b6128c225913d1fe8acda
huyhoc1310 <huyhoc1310@gmail.com>
2017-05-08 00:23:28 +0700

    update online test component

--- Nhom-Corgi/project/src/app/onlinetest.component.ts
+++ Nhom-Corgi/project/src/app/onlinetest.component.ts
@@ -1,38 +1,40 @@
 import { Component } from '@angular/core';
-import { OnlineTestService} from './services/onlinetest.service';
+import { OnlineTestService } from './services/onlinetest.service';
 @Component({
-    selector:'my-exam',
-    templateUrl:'onlinetest.component.html',
-    providers: [OnlineTestService]
+  selector: 'my-exam',
+  templateUrl: 'onlinetest.component.html',
+  providers: [OnlineTestService]
 })
-export class OnlineTestComponent{
-    public questions: Object[];
-    constructor(private onlinetestService: OnlineTestService){
+export class OnlineTestComponent {
+  questions: Object[];
+  correct: number;
 
-    }
-    ngOnInit(){
-        this.onlinetestService.GetList().subscribe((response:any)=>{
-            this.questions=response;
-        });
-    }
-    countCorrectAnswer: number;
-    saveStatus(question: Object, value:String) {
-      question['option'] = value;
-    }
-    saveValue(question: Object, value: String){
-      question['option'] = value;
-    }
-    check() {
-      this.countCorrectAnswer = 0;
+  show = false;
+  constructor(private onlinetestService: OnlineTestService) {
 
-      for (let i = 1; i < 51; i ++){
-        if(this.questions[i]['option'] == this.questions[i]['correct_answer']){
-          this.countCorrectAnswer++;
-          this.questions[i]['status'] = "Right";
-        }else{
-          this.questions[i]['status'] = "Wrong! The correct answer is " + this.questions[i]['correct_answer'];
-        }
+  }
+  ngOnInit() {
+    this.onlinetestService.GetList().subscribe((response: any) => {
+      this.questions = response;
+    });
+  }
+
+  saveStatus(question: Object, choice: String) {
+    question['choice'] = choice;
+  }
+
+  check() {
+    this.correct = 0;
+    for (let i = 0; i < this.questions.length; i++) {
+       console.log(this.questions[i]['choice']);
+      if (this.questions[i]['choice'] == this.questions[i]['correct_answer']) {
+       
+        this.correct++;
+        this.questions[i]['status'] = "correct";
+      } else {
+        this.questions[i]['status'] = "incorrect";
       }
-      console.log('dmmay');
     }
-}  
+    this.show = true;
+  }
+}
