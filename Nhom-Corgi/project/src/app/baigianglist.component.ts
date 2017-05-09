b79ea05730d6934238e5e491af961c411df6eea3
CaoMaiHuong <caomaihuongz97@gmail.com>
2017-05-10 00:03:35 +0700

    cập nhật dự án nhóm

---
+++ Nhom-Corgi/project/src/app/baigianglist.component.ts
@@ -0,0 +1,29 @@
+import { Component, OnInit } from '@angular/core';
+import { BaigiangService } from './services/baigiang.service';
+import { Router, ActivatedRoute } from "@angular/router";
+@Component({
+    selector: 'list-video-app',
+    templateUrl: './baigianglist.component.html',
+    providers: [BaigiangService]
+
+})
+
+export class BaigiangListComponent implements OnInit {
+      public baigiang: any[];
+    constructor(private baigiangService: BaigiangService) {
+
+    }
+    ngOnInit() {
+       this.baigiangService.GetList().subscribe((response: any) => {
+            this.baigiang = response;
+            console.log(response);
+    });
+  
+
+}
+}
+
+
+
+
+
