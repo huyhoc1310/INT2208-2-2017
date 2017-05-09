2b9befaf4862a30c8dde8235e7a16b88048f74bb
CaoMaiHuong <caomaihuongz97@gmail.com>
2017-05-09 23:59:09 +0700

    cập nhật video component

---
+++ Nhom-Corgi/project/src/app/baigiang.component.ts
@@ -0,0 +1,41 @@
+import { Component, OnInit, OnDestroy } from '@angular/core';
+import { BaigiangService } from "./services/baigiang.service"
+import { Router, ActivatedRoute } from "@angular/router";
+import { Subscription } from 'rxjs';
+import { Pipe, PipeTransform } from '@angular/core';
+import { DomSanitizer} from '@angular/platform-browser';
+
+@Pipe({ name: 'safe' })
+export class SafePipe implements PipeTransform {
+  constructor(private sanitizer: DomSanitizer) {}
+  transform(url) {
+    return this.sanitizer.bypassSecurityTrustResourceUrl(url);
+  }
+} 
+@Component({
+	selector: 'video-app',
+	templateUrl: './baigiang.component.html',
+	providers: [BaigiangService]
+})
+
+export class BaigiangComponent implements OnInit, OnDestroy {
+	public _id: number;
+	public subscription: Subscription;
+	public bg: any;
+	constructor(private router: Router, private activatedRoute: ActivatedRoute, public baigiangService: BaigiangService) {
+
+}
+	ngOnInit() {
+		this.subscription = this.activatedRoute.params.subscribe(params => {
+			this._id = params['id'];
+		});
+		this.baigiangService.GetSingle(this._id).subscribe((data) => {
+			this.bg = data;
+		});
+	}
+	ngOnDestroy() {
+		this.subscription.unsubscribe();
+	}
+
+
+}
